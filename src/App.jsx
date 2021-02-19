00a6b0d217e9b152416109ba1335702059570bbd
Kiz Morgan <kizimo@gmail.com>
2021-02-19 01:34:39 -0500

    finished?

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import "./styles.scss";
 
 import fetchLight from "./fetchLight";
 
+const lightColorArray = ["-red", "-yellow", "-green"];
+const rev = lightColorArray.reverse();
+
 export default function App() {
-  return <div />;
+  const [color, setColor] = useState("");
+  const [rnd] = useState(Math.floor(Math.random() * lightColorArray.length));
+  const [checked, setChecked] = useState(false);
+  const [intrvl, setIntrvl] = useState();
+  const [buttonName, setButtonName] = useState("Change");
+
+  const randomClick = useCallback(() => {
+    setColor(lightColorArray[rnd]);
+  }, [rnd]);
+
+  const buttonClick = async () => {
+    if (checked) {
+      let i = 0;
+      setColor(`${rev[i]}`);
+      setIntrvl(
+        setInterval(function () {
+          i = (i + 1) % rev.length;
+          setColor(`${rev[i]}`);
+        }, 2500)
+      );
+    } else {
+      const clr = await fetchLight();
+      setColor(`-${clr}`);
+    }
+  };
+
+  const toggleCheck = (e) => {
+    setChecked(e.target.checked);
+    if (e.target.checked) {
+      setButtonName("Start");
+    } else {
+      clearInterval(intrvl);
+      setIntrvl();
+      setButtonName("Change");
+    }
+  };
+
+  const light = (clr, lgt) => {
+    clr = clr === lgt ? clr : "";
+    return (
+      <div className="circle-cap">
+        <div className={`circle${clr}`} />
+      </div>
+    );
+  };
+
+  return (
+    <div className="app">
+      <div className="square" onClick={(e) => randomClick()}>
+        {light(color, "-red")}
+        {light(color, "-yellow")}
+        {light(color, "-green")}
+      </div>
+      <button onClick={(e) => buttonClick()}>{buttonName}</button>
+      <span>
+        <input
+          type="checkbox"
+          id="toggle"
+          className="toggle"
+          onChange={(e) => toggleCheck(e)}
+        />
+        <label htmlFor="toggle" className="switch"></label>
+      </span>
+    </div>
+  );
 }
--- src/fetchLight.js
+++ src/fetchLight.js
@@ -1,5 +1,10 @@
-export default function fetchLight() {
-  return new Promise((resolve, reject) => {
-    resolve("red");
-  });
+export default async function fetchLight() {
+  const res = await fetch("https://traffic-light-api.herokuapp.com");
+  if (res.status === 200) {
+    console.log("res", res);
+    const data = await res.json();
+    return data.color;
+  } else {
+    throw new Error("fetch no workie");
+  }
 }
