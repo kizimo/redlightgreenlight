4aefe6ff75efff1f1e541fbe40e9d3a820711f21
Kiz Morgan <kizimo@gmail.com>
2021-02-18 22:33:10 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+import fetchLight from "./fetchLight";
+
+export default function App() {
+  return <div />;
+}
---
+++ src/fetchLight.js
@@ -0,0 +1,5 @@
+export default function fetchLight() {
+  return new Promise((resolve, reject) => {
+    resolve("red");
+  });
+}
